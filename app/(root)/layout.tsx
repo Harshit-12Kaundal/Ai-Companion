4f18db5f3fcbfcfddd9c8dd9e212a95b6e564b94
Harshit-12Kaundal <97866054+Harshit-12Kaundal@users.noreply.github.com>
2024-04-03 20:05:49 +0530

    adding navbar

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "@/components/Navbar"
+
+const layout = ({children} :{children :React.ReactNode}) => {
+  return (
+    <div className="h-full">
+        <Navbar/>
+        <main className="md:pl-20 pt-16 h-full">
+            {children}
+        </main>
+    </div>
+  )
+}
+
+export default layout
